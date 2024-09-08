baa8051fa220cbe18c169adbe3460f4111c24a96
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2024-09-08 17:14:23 +0300

    create and update

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,5 +1,6 @@
 import random
 import prompt
+from brain_games.invitation import invite_str
 
 
 welcome_string = 'Answer "yes" if the number is even, otherwise answer "no".'
@@ -9,27 +10,11 @@ def is_even(random_number):
     return random_number % 2 == 0 
 
 
-print('Welcome to the Brain Games!')
-name = prompt.string('May I have your name? ')
-print(f'Hello, {name}!')
-print(welcome_string)
-
-
-def res():
+def game():
 	number = random.randint(0, 99)
 	correct_answer = "yes" if is_even(number) else "no"
+	question = f'Question: {number}'
 	return number, correct_answer
 
 
-for i in range(0, 3):
-	number1, correct_answer1 = res()
-	print(f'Question: {number1}')
-	user_answer = input()
-	if user_answer == correct_answer1:
-		print('Correct!')
-	else:
-		print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer1}'.")
-		print(f"Let's try again, {name}!")
-		break
-	if i == 2:
-		print(f'Congratulations, {name}!')
+invite_str(game, welcome_string)
