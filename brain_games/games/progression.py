4d84cb265944365ddeaacee51850df96a48f8661
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2024-09-12 00:30:11 +0300

    fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,14 +12,15 @@ def oper_progression(start_prog, step_prog):
     result = random.choice(res_prog)
     index = res_prog.index(result)
     res_prog[index] = '..'
-    return res_prog, result
+    prog = ' '.join(map(str, res_prog))
+    return prog, result
 
 
 def game():
     start_prog = random.randint(1, 9)
     step_prog = random.randint(1, 9)
-    res_prog, result = oper_progression(start_prog, step_prog)
-    question = f'Question: {res_prog}'
+    prog, result = oper_progression(start_prog, step_prog)
+    question = f'Question: {prog}'
     return question, str(result)
 
 
