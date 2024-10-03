480cebc67d8868df1e332bed76f203ea9932b567
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2024-10-03 16:54:18 +0300

    brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,13 +7,16 @@ WELCOME_STRING = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(number):
     prime_row_base = [2, 3, 5, 7]
     result = '-'
+
     if number in prime_row_base and number != 2:
         result = 'yes'
         return result
+
     for itera in prime_row_base:
         if number % itera == 0:
             result = 'no'
             break
+    
     if result == '-':
         result = 'yes'
     return result
