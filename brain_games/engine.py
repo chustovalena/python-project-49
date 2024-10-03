f21ba394edd7eb1320e86c08b6031bc6f50a2e8b
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2024-10-03 16:53:15 +0300

    update  brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,8 +4,10 @@ import prompt
 def engine_func(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
+    
     print(f'Hello, {name}!')
     print(game.WELCOME_STRING)
+
     for i in range(0, 3):
         question, res = game.game()
         print(question)
