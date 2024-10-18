a86fb7d0ee0a9ffbaa589301cde530ca4a71fd5f
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2024-10-18 22:32:05 +0300

    update brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,6 +1,9 @@
 import prompt
 
 
+ROUNDS = 3
+
+
 def invite_str():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
@@ -11,16 +14,14 @@ def invite_str():
 def engine_func(game):
     name = invite_str()
     print(game.WELCOME_STRING)
-    for i in range(0, 3):
+    for _ in range(ROUNDS):
         question, res = game.game()
         print(question)
         user = input()
         print(f'Your answer: {user}')
-        if user == res:
-            print('Correct!')
-        else:
-            print(f'"{user}" is wrong answer ;(. Correct answer was "{res}".')
-            print(f"Let's try again, {name}!")
-            break
-        if i == 2:
-            print(f'Congratulations, {name}!')
+        if user != res:
+            return print(f'"{user}" is wrong answer ;(.'
+                         f' Correct answer was "{res}".'
+                         f'\nLet\'s try again, {name}!')
+        print('Correct!')
+    return print(f'Congratulations, {name}!')
