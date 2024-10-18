565e8be0069367c1d29ca961fdd42c2bddbdc18b
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2024-10-18 22:17:00 +0300

    udate brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,22 +1,24 @@
 import random
+import math
 
 
 WELCOME_STRING = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    prime_row_base = [2, 3, 5, 7]
-    result = '-'
-    if number in prime_row_base and number != 2:
-        result = 'yes'
-        return result
-    for itera in prime_row_base:
-        if number % itera == 0:
-            result = 'no'
-            break
-    if result == '-':
-        result = 'yes'
-    return result
+    res = '-'
+    if number <= 1:
+        res = 'no'
+        return res
+    number_sqrt = int(math.sqrt(number)) 
+    divisors = range(2, (number_sqrt + 1))
+    for element in divisors:
+        if number % element == 0:
+            res = 'no'
+            return res
+    if res == '-':
+        res = 'yes'
+    return res
 
 
 def game():
